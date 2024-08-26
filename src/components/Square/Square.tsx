52e8ea422a853fa34efc9e69fbd982d486093094
Rajat Patel <it.nodejs3@ampersandgroup.in>
2024-08-27 03:18:33 +0530

    feature/cricle-cross-added

--- src/components/Square/Square.tsx
+++ src/components/Square/Square.tsx
@@ -1,51 +1,74 @@
+import { useState } from "react";
 
-// const circleSvg = (
-//   <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-//     <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-//     <g
-//       id="SVGRepo_tracerCarrier"
-//       stroke-linecap="round"
-//       stroke-linejoin="round"
-//     ></g>
-//     <g id="SVGRepo_iconCarrier">
-//       {" "}
-//       <path
-//         d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
-//         stroke="#ffffff"
-//         stroke-width="2"
-//         stroke-linecap="round"
-//         stroke-linejoin="round"
-//       ></path>{" "}
-//     </g>
-//   </svg>
-// );
+const circleSvg = (
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+      stroke="#ffffff"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+  </svg>
+);
 
-// const crossSvg = (
-//   <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-//     <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-//     <g
-//       id="SVGRepo_tracerCarrier"
-//       stroke-linecap="round"
-//       stroke-linejoin="round"
-//     ></g>
-//     <g id="SVGRepo_iconCarrier">
-//       {" "}
-//       <path
-//         d="M19 5L5 19M5.00001 5L19 19"
-//         stroke="#fff"
-//         stroke-width="1.5"
-//         stroke-linecap="round"
-//         stroke-linejoin="round"
-//       ></path>{" "}
-//     </g>
-//   </svg>
-// );
+const crossSvg = (
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M19 5L5 19M5.00001 5L19 19"
+      stroke="#fff"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+  </svg>
+);
 
-const Square = () => {
+type SquareProps = {
+  id: number;
+  gameState: (string | number)[][];
+  currentElement: string | number;
+  currentPlayer: "circle" | "cross";
+  setCurrentPlayer: React.Dispatch<React.SetStateAction<"circle" | "cross">>;
+  setGameState: React.Dispatch<React.SetStateAction<(string | number)[][]>>;
+};
+
+const Square: React.FC<SquareProps> = ({
+  id,
+  gameState,
+  currentElement,
+  currentPlayer,
+  setCurrentPlayer,
+  setGameState,
+}) => {
+  const [icon, setIcon] = useState<JSX.Element | null>(null);
+
+  const clickOnSquare = () => {
+    if (!icon) {
+      const row = Math.floor((id - 1) / 3);
+      const col = (id - 1) % 3;
+
+      if (gameState[row][col] === id) {
+        setIcon(currentPlayer === "circle" ? circleSvg : crossSvg);
+
+        const newGameState = gameState.map((arr, i) =>
+          arr.map((el, j) => (i === row && j === col ? currentPlayer : el))
+        );
+        setGameState(newGameState);
+
+        setCurrentPlayer(currentPlayer === "circle" ? "cross" : "circle");
+      }
+    }
+  };
+
   return (
-    <div className=' w-[80px] h-[80px] square bg-slate-600 rounded-md'>
+    <div
+      className="w-[80px] h-[80px] square bg-slate-600 rounded-md flex justify-center items-center"
+      onClick={clickOnSquare}
+    >
+      {icon}
     </div>
-  )
-}
+  );
+};
 
-export default Square
+export default Square;
